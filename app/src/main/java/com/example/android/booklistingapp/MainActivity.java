1b97b29d05b5e97c014c124226da3832370f63f2
da7th - Moe <m.da7th@gmail.com>
2016-07-18 01:17:31 +0300

    added some code to check the types of connection currently available and to show it in the textView instead of the title

--- app/src/main/java/com/example/android/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.booklistingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,20 +13,35 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextView;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //this is to check if the wifi or mobile data services are connected
+        final String DEBUG_TAG = "NetworkStatusExample";
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        boolean isWifiConn = networkInfo.isConnected();
+        networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        boolean isMobileConn = networkInfo.isConnected();
+        Log.d(DEBUG_TAG, "Wifi connected: " + isWifiConn);
+        Log.d(DEBUG_TAG, "Mobile connected: " + isMobileConn);
+
+        tv = (TextView) findViewById(R.id.page_title);
+        tv.setText("Wifi Connected:" + isWifiConn + "\nMobile connected:" + isMobileConn);
 
     }
+
 
     public void searchBooks(View view){
         editTextView = (EditText) findViewById(R.id.edit_text_view);
         String example = editTextView.getText().toString();
 
-        TextView tv = (TextView) findViewById(R.id.page_title);
+        tv = (TextView) findViewById(R.id.page_title);
         tv.setText(example);
     }
 }
